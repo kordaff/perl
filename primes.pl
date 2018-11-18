cb816b5adca95fd576f77d2a45948ec052f3bffe
Phillip Hartfield Sr <phil681@yahoo.com>
2018-11-18 08:59:35 -0700

    Fixed to actually work.  Verified pi(10,000,000)
    pi(10,000,000) is the number of primes under 10,000,000
    This program calculates 664579 for pi(10,000,000)
    To get the first 1.5 million primes took about 4300 MB of memory - factored nearly 25,000,000 at the same time before I axed the run.
    
    1536000 prime numbers recorded in 545 seconds   24490129
    
    last line in factors.txt:
    factors of 24499670: 2,5,13,188459

--- primes.pl
+++ primes.pl
@@ -1,15 +1,35 @@
 #!C:\Perl\bin\perl.exe
 # $Id$  $Revision$  $HeadURL$  $Date$
 #
-# Starting with the first 9 prime numbers, this program calculates whether
-# a number is prime, or what its prime factors are, up to 10,000,000.
+# replace line 1 with #!/usr/bin/perl on Linux/Unix  
 #
-# should be named something like prime_factors.pl instead eh
+# Previous version was incorrect.  It wasn't adding new primes to @primes
+# Fixed that.
+#
+# v1.1 holds all the primes it calculates in memory.  I set it to calculate
+# primes up to 100,000,000 but by 1.5 million primes, it was using 4300 MB of
+# memory
+#
+# Need to make a change in next version to only hold up to $max of highest 
+# number it will be factoring/checking for primeness.
+#
+# Starting with the first 9 prime numbers, this program calculates whether
+# a number is prime, or what its prime factors are, up to 100,000,000.
+#
+# should be named something like primes_and_factors.pl instead eh
+#
+# I did modify it in v1.1 to output two files, one with primes, one with
+# factored non-primes.
+#
+# pi(1000)       = 168
+# pi(10_000_000) = 664579
+#
+# ie: number of primes by 1000 and 10_000_000 (ten million, Perl likes _'s)
+# program now verified to calculate correct number of primes.
 #
 use strict;
 use warnings;
-our $VERSION=1.0;
-use File::Slurp qw(read_file);
+our $VERSION=1.1;
 
 my %factor;
 my @primes=(2,3,5,7,9,11,13,17,19);
@@ -19,20 +39,22 @@ my $count=0;
 my $p=1000;
 my $t0=time();
 
-open(my $fh,">","primes_and_factors.txt") or die $!;
-while( $n <= (10000000) )
+open(my $fh1,">","primes.txt") or die $!;
+open(my $fh2,">","factors.txt") or die $!;
+while( $n <=  100_000_000 )
   {
   check_number($n);
   $n++;
   }
-close $fh;
+close $fh1;
+close $fh2;
 
 sub check_number
   {
   my $num=shift;
   my $max=sqrt($num);
   my $flag=0;
-  for my $i (0..$#primes)
+  for my $i (0..int($max))
     {
 #     print "checking $num/$primes[$i]";
     my $this=$primes[$i];
@@ -41,7 +63,7 @@ sub check_number
       {
       $flag=1;
       my $div=$num/$this;
-      print $fh "factors of $num: $this,$factor{$div}\n";
+      print $fh2 "factors of $num: $this,$factor{$div}\n";
       $factor{$num}="$this,$factor{$div}";
       last;
       }
@@ -49,8 +71,16 @@ sub check_number
   if (!$flag)
     {
     $count++;
-    print $fh "$num\n";
-    # print "$num is prime\n";
+    print $fh1 "$num\n";
+    if ($num > $primes[$#primes] )
+      {
+      @primes[$#primes+1]=$num;
+      # print "$num is prime - added to array\n";
+      }
+    else
+      {
+      # print "$num is prime - already in array\n";
+      }
     $factor{$num}="$num";
     if ($count/$p == int($count/$p))
       {
@@ -58,4 +88,4 @@ sub check_number
       print "$count prime numbers recorded in $elapsed seconds   $num\n";
       }
     }
-  }
+}
